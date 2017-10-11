a8e800df785dcf1c71d9d5f7a099316e42b81d24
Hubert Zhang <hubertzhang@apache.org>
2017-10-11 14:51:17 +0800

    exit when git describe failed.

--- concourse/scripts/build_plcontainer.sh
+++ concourse/scripts/build_plcontainer.sh
@@ -19,6 +19,9 @@ build_plcontainer() {
   # build plcontainer
   pushd plcontainer_src
   if [ "${DEV_RELEASE}" == "release" ]; then
+      if git describe >/dev/null 2>&1 ; then
+          echo "git describe failed" || exit 1
+      fi
       PLCONTAINER_VERSION=$(git describe | awk -F. '{printf("%d.%d", $1, $2)}')
       PLCONTAINER_RELEASE=$(git describe | awk -F. '{print $3}')
   else
