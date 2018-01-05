337270f0bd1932d070f182e6f95b8e91d7ef9f71
Hubert Zhang <hubertzhang@apache.org>
2018-01-05 10:11:01 +0800

    Add license for elog.c

--- src/common/comm_utils.c
+++ src/common/comm_utils.c
@@ -1,9 +1,8 @@
 /*------------------------------------------------------------------------------
  *
  *
- * Copyright (c) 2016, Pivotal.
+ * Portions Copyright (c) 2016, Pivotal.
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
- * Portions Copyright (c) 2005-2009, Greenplum inc
  *
  *------------------------------------------------------------------------------
  */
