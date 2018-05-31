bb35f793ce301a5909c33ed089d77442a103d438
Haozhou Wang <hawang@pivotal.io>
2018-05-31 04:41:56 -0400

    add check

--- concourse/scripts/docker_build_image.sh
+++ concourse/scripts/docker_build_image.sh
@@ -42,6 +42,7 @@ docker_build() {
 	tar -zxvf DataScience*.gppkg; \
 	chmod +x *.sh; \
 	cp /usr/local/greenplum-db/lib/libstdc++.so.6 .
+	ls -lh
 	docker build -f Dockerfile.$language -t pivotaldata/plcontainer_${language}_shared:devel ./ ; \
 	popd; \
 	docker save pivotaldata/plcontainer_${language}_shared:devel | gzip -c > ~/${IMAGE_NAME}; \
