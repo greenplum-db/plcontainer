84fae5afa4e4fa5e324b4386737ddf4a6c1438dd
Weinan Wang <wewang@pivotal.io>
2019-05-10 17:18:45 +0800

    package done

--- package/gppkg-deb.mk
+++ package/gppkg-deb.mk
@@ -13,25 +13,32 @@ endif
 DEB_ARGS=$(subst -, ,$*)
 DEB_NAME=$(word 1,$(RPM_ARGS))
 PWD=$(shell pwd)
+TARGET_GPPKG=$(PLC_GPPKG)
 CONTROL_NAME=plcontainer.control.in
-%.deb: 
+
+.PHONY: pkg
+pkg: $(TARGET_GPPKG)
+
+%.deb:
 	rm -rf UBUNTU
 	mkdir UBUNTU/DEBIAN -p
+	mkdir UBUNTU/share/postgresql/extension/ -p
+	cat gppkg_spec.yml.in | sed "s/#arch/$(ARCH)/g" | sed "s/#os/$(MAJOR_OS)/g" | sed "s/#gpver/$(GP_VERSION_NUM)/g" | sed "s/#plcver/$(PLC_GPPKG_VER)/g" > gppkg_spec.yml
 	cat $(PWD)/$(CONTROL_NAME) | sed -r "s|#version|$(PLC_GPPKG_VER)|" | sed -r "s|#arch|$(ARCH)|" > $(PWD)/UBUNTU/DEBIAN/control
 	$(MAKE) -C $(PLC_DIR) install DESTDIR=$(PWD)/UBUNTU bindir=/bin libdir=/lib/postgresql pkglibdi=/lib/postgresql datadir=/share/postgresql
 	dpkg-deb --build UBUNTU $@
 
-%.gppkg: $(MAIN_PKG) $(DEPENDENT_PKGS)
+%.gppkg: $(PLC_DEB) $(DEPENDENT_DEBS)
 	echo "SHELL $(SHELL), pwd=`pwd`, ls=`ls`"
-	mkdir -p gppkg/deps 
+	mkdir -p gppkg/deps
 	cp gppkg_spec.yml gppkg/
-	cp $(MAIN_PKG) gppkg/ 
+	cp $(PLC_DEB) gppkg/
 ifdef DEPENDENT_PKGS
-	for dep_rpm in $(DEPENDENT_PKGS); do \
+	for dep_rpm in $(DEPENDENT_DEBS); do \
 		cp $${dep_pkg} gppkg/deps; \
 	done
 endif
-	source $(GPHOME)/greenplum_path.sh && gppkg --build gppkg 
+	/bin/bash -c "source $(GPHOME)/greenplum_path.sh && gppkg --build gppkg"
 	rm -rf gppkg
 
 clean:
@@ -43,6 +50,6 @@ ifdef EXTRA_CLEAN
 endif
 
 install: $(TARGET_GPPKG)
-	source $(INSTLOC)/greenplum_path.sh && gppkg -i $(TARGET_GPPKG)
+	/bin/bash source -c "$(INSTLOC)/greenplum_path.sh && gppkg -i $(TARGET_GPPKG)"
 
 .PHONY: install clean
