7c688a15644d195121707594343c0f327948af5e
Haozhou Wang <hawang@pivotal.io>
2019-12-17 10:44:06 +0800

    Add gluonts module and update regress answer file

--- concourse/scripts/run_plcontainer_tests.sh
+++ concourse/scripts/run_plcontainer_tests.sh
@@ -27,13 +27,15 @@ export MASTER_DATA_DIRECTORY=/data/gpdata/master/gpseg-1; \
 source /usr/local/greenplum-db-devel/greenplum_path.sh; \
 plcontainer image-add -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python-images.tar.gz; \
 plcontainer image-add -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-r-images.tar.gz; \
+plcontainer image-add -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer-python3-images.tar.gz; \
 plcontainer runtime-add -r plc_python_shared -i pivotaldata/plcontainer_python_shared:devel -l python -s use_container_logging=yes; \
-plcontainer runtime-add -r plc_python3_shared -i pivotaldata/plcontainer_python_shared:devel -l python3 -s use_container_logging=yes; \
+plcontainer runtime-add -r plc_python3_shared -i pivotaldata/plcontainer_python3_shared:devel -l python3 -s use_container_logging=yes; \
 plcontainer runtime-add -r plc_r_shared -i pivotaldata/plcontainer_r_shared:devel -l r -s use_container_logging=yes; \
 plcontainer runtime-add -r plc_python_shared_oom -i pivotaldata/plcontainer_python_shared:devel -l python -s use_container_logging=yes -s memory_mb=100; \
 gpstop -arf; \
 psql -d postgres -f /usr/local/greenplum-db-devel/share/postgresql/plcontainer/plcontainer_install.sql; \
 pushd plcontainer_src/tests; \
 timeout -s 9 60m make tests; \
+timeout -s 9 60m make testpy3; \
 popd; \
 \""
