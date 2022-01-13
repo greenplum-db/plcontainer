476c3e3a3e983617dee933ec83d0566dfa6df996
RMT <d.rong@outlook.com>
2022-01-13 19:18:42 +0800

    wip try to solve the error installing docker again again again

--- concourse/scripts/docker_install.sh
+++ concourse/scripts/docker_install.sh
@@ -50,17 +50,15 @@ install_docker() {
         ;;
       rhel8)
           # Install cpio for gppkg
-          ssh rhel@$node "bash -c \" \
+          ssh rhel@$node "
             sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo; \
+            sleep 5; \
             sudo dnf install -y docker-ce; \
-            sudo dnf install -y docker-ce-cli; \
-            sudo dnf install -y containerd.io; \
             sudo groupadd docker; \
             sudo usermod -a -G docker gpadmin; \
             sudo newgrp docker; \
             sudo systemctl start docker; \
             sudo yum install -y cpio; \
-          \"
           "
           ;;
       ubuntu18)
