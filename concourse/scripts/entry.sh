9081b60126abca3a9ed6bfd4cdf3e94d25706252
yihong0618 <zouzou0208@gmail.com>
2022-07-08 17:39:35 +0800

    fix: no --yes for docker install

--- concourse/scripts/entry.sh
+++ concourse/scripts/entry.sh
@@ -173,7 +173,7 @@ function install_extra_build_dependencies() {
         apt install -y apt-transport-https ca-certificates curl software-properties-common
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
         add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-        apt install docker-ce
+        apt install -y docker-ce
         ;;
 
     *) ;;
