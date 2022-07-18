c40a722b0a3c63243487481866017cc2d94262d0
Sasasu <i@sasa.su>
2022-07-18 14:22:25 +0800

    strict permission check when user enables network or GPU.
    if GPU or network not enabled, empty roles list means any user can use
    this runtime.
    
    if GPU or network enable, empty roles list means no one can use this
    runtime.

--- tests/regress/sql/test_config_gpu.sql
+++ tests/regress/sql/test_config_gpu.sql
@@ -1,10 +1,11 @@
 -- start_ignore
-\! plcontainer runtime-add -r plc_python3_shared_gpu -i python39.alpine -l python3
+\! plcontainer runtime-add -r plc_python3_shared_gpu -i python39.alpine:latest -l python3 -s roles=$USER
 \! plcontainer runtime-backup -f test_config_gpu.xml
-\! ./test_assign_gpu.py --file ./test_config_gpu.xml -i --runtime plc_python3_shared_gpu --action all
+\! ./data/test_assign_gpu.py --file ./test_config_gpu.xml -i --runtime plc_python3_shared_gpu --action all
 \! plcontainer runtime-restore -f test_config_gpu.xml
 \! rm test_config_gpu.xml
 -- end_ignore
+select * from plcontainer_refresh_config;
 
 create function with_conf_gpu() returns int as $$
   # container: plc_python3_shared_gpu
@@ -12,6 +13,26 @@ create function with_conf_gpu() returns int as $$
 $$ LANGUAGE plcontainer;
 
 -- gpu=all means assign all available to this runtime. and not emit error when the GPU is not exist
+-- start_ignore
+-- TODO
+-- Failed to start container return code: 500, detail: {"message":"could not select device driver \"\" with capabilities: [[gpu]]"}
+select * from with_conf_gpu();
+-- end_ignore
+
+-- start_ignore
+\! plcontainer runtime-delete -r plc_python3_shared_gpu
+-- end_ignore
+
+-- start_ignore
+\! plcontainer runtime-add -r plc_python3_shared_gpu -i python39.alpine:latest -l python3
+\! plcontainer runtime-backup -f test_config_gpu.xml
+\! ./data/test_assign_gpu.py --file ./test_config_gpu.xml -i --runtime plc_python3_shared_gpu --action all
+\! plcontainer runtime-restore -f test_config_gpu.xml
+\! rm test_config_gpu.xml
+-- end_ignore
+select * from plcontainer_refresh_config;
+
+-- strict permission check, except error
 select * from with_conf_gpu();
 
 drop function with_conf_gpu();
