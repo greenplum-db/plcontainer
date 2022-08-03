8ed0c057f96c2208d4375fee8a3668a9360a4d59
yihong0618 <zouzou0208@gmail.com>
2022-08-03 10:24:24 +0800

    fix: pipeline failed

--- concourse/scripts/test_plcontainer_py3.sh
+++ concourse/scripts/test_plcontainer_py3.sh
@@ -14,7 +14,8 @@ function _main() {
     time plcontainer runtime-add -r plc_python_shared -i "${CONTAINER_NAME_SUFFIX_PYTHON}" -l python3
     time plcontainer runtime-add -r plc_python_shared_oom -i "${CONTAINER_NAME_SUFFIX_PYTHON}" -l python3 -s memory_mb=100
     time cmake --build . --target testpy3 
-    if [[ ${CONTAINER_NAME_SUFFIX_PYTHON} == *_b ]]; then 
+    if [[ ${CONTAINER_NAME_SUFFIX_PYTHON} == *_b ]]; then
+        time gpstop -ar
         time cmake --build . --target testpy3_bundle 
     fi
     # Test gppkg uninstall
