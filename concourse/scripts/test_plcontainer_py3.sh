9a1a17ffc722d21b59e2a181274558377f41fac2
yihong0618 <zouzou0208@gmail.com>
2022-09-23 15:17:20 +0800

    fix: try the test

--- concourse/scripts/test_plcontainer_py3.sh
+++ concourse/scripts/test_plcontainer_py3.sh
@@ -19,6 +19,8 @@ function _main() {
     gppkg -q --all
     # Find the package name
     local pkg_name
+    # for test
+    exit 1
     pkg_name=$(gppkg -q --all | awk -F"[-]+" '/plcontainer/{print $1}')
     # Uninstall it
     gppkg -r "${pkg_name}"
