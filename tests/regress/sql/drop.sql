7b73da087a28b7458f7a757f243d710f228d2e4a
Xuebin Su <sxuebin@vmware.com>
2023-03-29 22:23:33 -0400

    Drop database

--- tests/regress/sql/drop.sql
+++ tests/regress/sql/drop.sql
@@ -1,1 +1,8 @@
 DROP EXTENSION plcontainer CASCADE;
+
+SELECT current_database(), pg_backend_pid() \gset
+\c postgres
+SELECT pg_terminate_backend(:pg_backend_pid);
+-- Sleep until the container is killed by the child process of QD/QE
+\! sleep 5
+DROP DATABASE :current_database;
