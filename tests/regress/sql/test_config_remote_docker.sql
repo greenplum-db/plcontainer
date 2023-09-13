796b25896160df3fd9199fd8deb6f360f5d919a3
RMT <d.rong@outlook.com>
2023-09-13 15:25:03 +0800

    add test cases

---
+++ tests/regress/sql/test_config_remote_docker.sql
@@ -0,0 +1,26 @@
+-- start_matchignore
+-- m/enable_network/
+-- end_matchignore
+
+-- start_ignore
+\! plcontainer runtime-add -r plc_python3_shared_remote_docker -i ${CONTAINER_NAME_SUFFIX_PYTHON} -l python3
+\! plcontainer runtime-backup -f test_config_remote_docker.xml
+\! ./data/test_assign_backend.py ./test_config_remote_docker.xml -r plc_python3_shared_remote_docker
+\! plcontainer runtime-restore -f test_config_remote_docker.xml
+\! rm test_config_remote_docker.xml
+-- end_ignore
+select * from plcontainer_refresh_config;
+
+create function with_conf_remote_docker() returns TEXT as $$
+  # container: plc_python3_shared_remote_docker
+  return 'hello'
+$$ LANGUAGE plcontainer;
+
+select * from with_conf_remote_docker();
+
+-- start_ignore
+\! plcontainer runtime-delete -r plc_python3_shared_remote_docker
+-- end_ignore
+
+drop function with_conf_remote_docker;
+
