greenplum-db/plcontainer

commit 699da5c71c132cb8ceede775000f65eb87342f5f

Revert "Revert "Support remote docker (#630)" (#700)" (#705)
This reverts commit 8beaf74aa27c75e50bfcf745ab6dd6ade176f3c1.

src/plc_process_api.c
@@ -27,18 +27,20 @@
 #include <json-c/json.h>
 #include <sys/wait.h>
 
-int plc_process_create_container(runtimeConfEntry *conf, char **name, int container_id, char **uds_dir) {
-    (void)(name);
-    bool has_error;
-    char *volumeShare = get_sharing_options(conf, container_id, &has_error, uds_dir);
-    (void)(volumeShare);
-/*
- *  no shared volumes should not be treated as an error, so we use has_error to
- *  identifier whether there is an error when parse sharing options.
- */
-    if (has_error == true) {
+int plc_process_create_container(
+		const runtimeConfEntry *conf,            // input the runtime config
+		const backendConnectionInfo *backend,    // input the backend connection info
+		const int container_slot,                // input the slot id used to generate uds name
+		runtimeConnectionInfo *connection        // output the new process connection info
+) {
+    // no shared volumes should not be treated as an error (TODO why?), so we use has_error to
+    // identifier whether there is an error when parse sharing options.
+    char *volumeShare;
+    bool has_error = generate_sharing_options_and_uds_address(conf, backend, container_slot, connection, &volumeShare);
+    if (has_error == true || volumeShare == NULL) {
         return -1;
     }
+    pfree(volumeShare);
 
     pid_t pid = -1;
     int res = 0;
@@ -74,47 +76,78 @@ int plc_process_create_container(runtimeConfEntry *conf, char **name, int contai
     }
 
     // parent, continue......
-    *name = palloc(64);
-    sprintf(*name, "%d", pid);
+    connection->identity = palloc(64);
+    snprintf(connection->identity, 64, "%d", pid);
 
-    backend_log(LOG, "create backend process with name:%s", *name);
+    backend_log(LOG, "create backend process with name:%s", connection->identity);
     return res;
 }
 
-int plc_process_start_container(const char *name) {
+int plc_process_start_container(
+		const backendConnectionInfo *backend,    // input the backend connection info
+		runtimeConnectionInfo *connection        // output the new process connection info
+) {
+	(void)backend;
+
     int res = 0;
-    backend_log(LOG, "start backend process with name:%s", name);
+    backend_log(LOG, "start backend process with name:%s", connection->identity);
     return res;
 }
 
-int plc_process_kill_container(const char *name) {
+int plc_process_kill_container(
+		const backendConnectionInfo *backend,    // input the backend connection info
+		const runtimeConnectionInfo *connection  // input the new process connection info
+) {
+	(void)backend;
+
     int res = 0;
-    int pid = atoi(name);
+    int pid = atoi(connection->identity);
+	Assert(pid != 0);
+
     kill(pid, SIGKILL);
-    backend_log(LOG, "kill backend process with name:%s", name);
+    backend_log(LOG, "kill backend process with name:%d", pid);
     return res;
 }
 
-int plc_process_inspect_container(const char *name, char **element, plcInspectionMode type) {
+int plc_process_inspect_container(
+		const plcInspectionMode type,            // the inspect method
+		const backendConnectionInfo *backend,    // input the backend connection info
+		const runtimeConnectionInfo *connection, // input the new process connection info
+		char **element                           // the output
+) {
+	(void)backend;
+
     int res = 0;
     *element = palloc(64);
-    sprintf(*element, "process:%s type:%d", name, type);
-    backend_log(LOG, "inspect backend process with name:%s", name);
+    sprintf(*element, "process:%s type:%d", connection->identity, type);
+    backend_log(LOG, "inspect backend process with name:%s", connection->identity);
     return res;
 }
 
-int plc_process_wait_container(const char *name) {
+int plc_process_wait_container(
+		const backendConnectionInfo *backend,    // input the backend connection info
+		const runtimeConnectionInfo *connection  // input the new process connection info
+) {
+	(void)backend;
+
     int res = 0;
-    int pid = atoi(name);
+    int pid = atoi(connection->identity);
     waitpid(pid, &res, 0);
-    backend_log(LOG, "wait backend process with name:%s", name);
+    backend_log(LOG, "wait backend process with name:%d", pid);
     return res;
 }
 
-int plc_process_delete_container(const char *name) {
+int plc_process_delete_container(
+		const backendConnectionInfo *backend,    // input the backend connection info
+		const runtimeConnectionInfo *connection  // input the new process connection info
+) {
+	(void)backend;
+
     int res = 0;
-    int pid = atoi(name);
+    int pid = atoi(connection->identity);
+	Assert(pid != 0);
+
     kill(pid, SIGKILL);
-    backend_log(LOG, "delete backend process with name:%s", name);
+    backend_log(LOG, "delete backend process with name:%d", pid);
     return res;
 }

tests/regress/pl_schedule_py
@@ -49,6 +49,7 @@ test: memory_parallel_python_1 memory_parallel_python_2 memory_parallel_python_3
 # test case for PL/Container configuration
 test: test_containernetwork
 test: test_config_gpu
+test: test_config_remote_docker
 
 # Drop the extension - need to be last
 test: drop

concourse/scripts/docker-lib.sh
@@ -88,7 +88,7 @@ start_docker() {
   try_start() {
     # we add this arg `--storage-driver`
     # Otherwise, if concourse worker use `btrfs`` files created inside the container will be leaked to the concourse worker host.
-    dockerd --storage-driver overlay2 --data-root $DOCKER_DATA_ROOT ${server_args} >$LOG_FILE 2>&1 &
+    dockerd --storage-driver overlay2 -H unix:///var/run/docker.sock -H tcp://127.0.0.1 --data-root $DOCKER_DATA_ROOT ${server_args} >$LOG_FILE 2>&1 &
     echo $! > /tmp/docker.pid
 
     sleep 1

dockerfiles/Dockerfile.python37.ubuntu1804
@@ -1,5 +1,6 @@
 FROM ubuntu:18.04
 
+EXPOSE 8080
 RUN apt update && apt install -y python3.7 python3.7-dev python3.7-distutils gcc g++ make wget
 RUN wget https://bootstrap.pypa.io/get-pip.py
 RUN ln -sf /usr/bin/python3.7 /usr/bin/python3

dockerfiles/Dockerfile.python39.alpine
@@ -4,6 +4,7 @@ FROM python:3.9-alpine3.13
 # That happens with the docker on CentOS7.
 # See more details on https://gitlab.alpinelinux.org/alpine/aports/-/issues/12321
 
+EXPOSE 8080
 RUN apk add --no-cache gcc g++ cmake make musl-dev
 
 RUN mkdir -p /clientdir

dockerfiles/Dockerfile.python39.ubuntu2204
@@ -1,4 +1,5 @@
 FROM ubuntu:22.04
+EXPOSE 8080
 ENV DEBIAN_FRONTEND noninteractive
 # this is to fix UnicodeDecodeError: 'ascii' codec for pymc and other packages for
 # UnicodeDecodeError Error, can check link: https://github.com/docker-library/python/issues/13

dockerfiles/Dockerfile.r.alpine
@@ -1,5 +1,6 @@
 FROM alpine:latest
 
+EXPOSE 8080
 RUN apk add --no-cache gcc g++ cmake make musl-dev R R-dev
 
 # for R test we need to make these `env`

commit f6218a4e151d7d5c60b37c4d1c7c14b04ab4a31f

Support using process as backend for Python 3 (#713)
Using a process on host as backend to run the client makes debugging
much easier. This is expected to be done by 
`SET plcontainer.backend_type='process';` However, in the prevous
releases, this only works for Python 2 and R but not Python 3.

This patch fixes this issue by adding Python 3 and a valid client,
and fix the wrong UDS path issue for the backend type "process".
The issue was introduced by commit 699da5c.

src/common/comm_server.c
@@ -77,6 +77,8 @@ static int start_listener_ipc() {
 	gid_t qe_gid;
 	long val;
 
+	mkdir(IPC_CLIENT_DIR, 0700);
+
 	/* filename: IPC_CLIENT_DIR + '/' + UDS_SHARED_FILE */
 	sz = strlen(IPC_CLIENT_DIR) + 1 + MAX_SHARED_FILE_SZ + 1;
 	uds_fn = pmalloc(sz);

src/plc_configuration.c
@@ -1106,6 +1106,7 @@ backendConnectionInfo *runtime_conf_copy_backend_connection_info(const backendCo
 			r->plcBackendRemoteDocker.password = plc_top_strdup_null(a->plcBackendRemoteDocker.password);
 			break;
 		case PLC_BACKEND_PROCESS:
+			break;
 		case PLC_BACKEND_UNIMPLEMENT:
 			Assert(!"not implement");
 			break;
@@ -1221,10 +1222,10 @@ runtimeConnectionInfo* runtime_conf_get_runtime_connection_info(const backendCon
 			r->connection_tcp.port = 0;
 			break;
 		case PLC_BACKEND_DOCKER:
+		case PLC_BACKEND_PROCESS:
 			r->tag = PLC_RUNTIME_CONNECTION_UDS;
 			r->connection_uds.uds_address = NULL;
 			break;
-		case PLC_BACKEND_PROCESS:
 		case PLC_BACKEND_UNIMPLEMENT:
 			Assert(!"unimplement");
 	}
@@ -1321,7 +1322,11 @@ int generate_sharing_options_and_uds_address(const runtimeConfEntry *conf, const
 		}
 
 		// do the format again to get the UDS filename
-		n = snprintf(_dirname, sz, "%s.%d.%d.%d/%s", IPC_GPDB_BASE_DIR, getpid(), uds_count, container_slot, UDS_SHARED_FILE);
+		if (backend->tag == PLC_BACKEND_PROCESS) {
+			n = snprintf(_dirname, sz, "%s/%s", IPC_GPDB_BASE_DIR, UDS_SHARED_FILE);
+		} else {
+			n = snprintf(_dirname, sz, "%s.%d.%d.%d/%s", IPC_GPDB_BASE_DIR, getpid(), uds_count, container_slot, UDS_SHARED_FILE);
+		}
 		Assert(n < sz);
 
 		// free out side
@@ -1475,6 +1480,7 @@ static void xml_read_backend(void *ctx, size_t off, xmlNode *node) {
 			});
 			break;
 		case PLC_BACKEND_PROCESS:
+			break;
 		case PLC_BACKEND_UNIMPLEMENT:
 			Assert(!"unreachable");
 			break;
@@ -1504,6 +1510,8 @@ static void parse_runtime_backend_configuration(xmlNode *root) {
 	plcBackend *new = MemoryContextAllocZero(TopMemoryContext, sizeof(plcBackend) * (old + 1));
 	new[0].name = plc_top_strdup("default");
 	new[0].tag = PLC_BACKEND_DOCKER;
+	if (plc_backend_type_string && strcmp(plc_backend_type_string, "process") == 0)
+		new[0].tag = PLC_BACKEND_PROCESS;
 	new[0].localdocker.uds_address = plc_top_strdup("/var/run/docker.sock");
 
 	memcpy(&new[1], runtime_backend_table, old * sizeof(plcBackend));

src/plc_process_api.c
@@ -54,8 +54,12 @@ int plc_process_create_container(
         } else {
             if (strstr(conf->command, "pyclient") != NULL) {
                 sprintf(binaryPath, "%s/bin/plcontainer_clients/pyclient", env_str);
-            } else {
+            } else if (strstr(conf->command, "rclient") != NULL) {
                 sprintf(binaryPath, "%s/bin/plcontainer_clients/rclient", env_str);
+            } else if (strstr(conf->command, "py3client") != NULL) {
+                sprintf(binaryPath, "%s/bin/plcontainer_clients/py3client", env_str);
+            } else {
+                plc_elog(ERROR, "Invalid command for runtime '%s'", conf->runtimeid);
             }
         }
         char uid_string[1024] = {0};
@@ -71,6 +75,15 @@ int plc_process_create_container(
             NULL
         };
 
+        /* Redirect log to file. */
+        char log_path[1024] = { 0 };
+        snprintf(log_path,  sizeof(log_path), "/tmp/plcontainer_client--%d.log", getpid());
+
+        int log_fd = open(log_path, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+        dup2(log_fd, 1);
+        dup2(log_fd, 2);
+        close(log_fd);
+
         execle(binaryPath, binaryPath, NULL, env);
         exit(EXIT_FAILURE);
     }
@@ -79,7 +92,7 @@ int plc_process_create_container(
     connection->identity = palloc(64);
     snprintf(connection->identity, 64, "%d", pid);
 
-    backend_log(LOG, "create backend process with name:%s", connection->identity);
+    backend_log(NOTICE, "created backend process with pid: %s", connection->identity);
     return res;
 }
 

src/plcontainer.h
@@ -11,6 +11,8 @@
 
 #include "fmgr.h"
 
+extern char *plc_backend_type_string;
+
 /* entrypoint for all plcontainer procedures */
 Datum plcontainer_call_handler(PG_FUNCTION_ARGS);
 

tests/regress/expected/backend_process.out
@@ -1,0 +1,24 @@
+\connect
+SET plcontainer.backend_type = 'process';
+CREATE OR REPLACE FUNCTION check_postgres() RETURNS bool AS $$
+# container: plc_python_shared
+import subprocess
+try:
+    output = subprocess.check_output(["ps", "-ef"], text=True, stderr=subprocess.STDOUT)
+    return "postgres" in output
+except subprocess.CalledProcessError as e:
+    raise Exception(e.stdout)
+$$ LANGUAGE plcontainer;
+SELECT check_postgres();
+ check_postgres 
+----------------
+ t
+(1 row)
+
+SHOW plcontainer.backend_type;
+ plcontainer.backend_type 
+--------------------------
+ process
+(1 row)
+
+\connect

tests/regress/pl_schedule_py
@@ -7,6 +7,10 @@ test: test_configuration
 # test declaration combinations in function definitions.
 test: runtimeid_declaration
 
+# Skipping this on CI for now since this feature is only for debugging and will
+# not be used in production.
+# test: backend_process
+
 # set R and Python function - need before "test PL/Container normal function"
 test: function_python function_python_gpdb6
 

tests/regress/sql/backend_process.sql
@@ -1,0 +1,19 @@
+\connect
+
+SET plcontainer.backend_type = 'process';
+
+CREATE OR REPLACE FUNCTION check_postgres() RETURNS bool AS $$
+# container: plc_python_shared
+import subprocess
+try:
+    output = subprocess.check_output(["ps", "-ef"], text=True, stderr=subprocess.STDOUT)
+    return "postgres" in output
+except subprocess.CalledProcessError as e:
+    raise Exception(e.stdout)
+$$ LANGUAGE plcontainer;
+
+SELECT check_postgres();
+
+SHOW plcontainer.backend_type;
+
+\connect